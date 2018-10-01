74ebb2bad3cc0f5eb467d3b8eb45a341dc1a926c
diegomora <Alexar89@gmail.com>
2018-10-01 16:33:42 -0500

    Commit of Firts Servlet Tomcat v7

---
+++ src/apress/helloworldServlet/HelloWorldServlet.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package apress.helloworldServlet;
+
+/**
+ * @author diegomora
+ *
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**Library for Servlet**/
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class HelloWorldServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+		
+		try {
+			
+			/**Response of content Type**/
+			response.setContentType("text/html");
+			PrintWriter printWriter = response.getWriter();
+			/****/
+			printWriter.println("<H2>");
+			printWriter.println("Hello World");
+			printWriter.println("</H2>");
+			
+		}catch (IOException ioException) {
+			// TODO: handle exception
+			
+		}
+		
+	}
+
+}
